8669d204262264e253463a33afac73f5ba2e8222
BuildTools <ercv106@hotmail.com>
2021-03-02 11:15:34 -0600

    02/03/2021 11:15:33

--- Act1-log_Analysis/Log_analysis.py
+++ Act1-log_Analysis/Log_analysis.py
@@ -5,79 +5,86 @@ from time import sleep, time
 import numpy as np
 import sys
 
-def get_random_sample_data(data:DataFrame, test_ratio:float=0.1):
+
+def get_random_sample_data(data: DataFrame, test_ratio: float = 0.1):
     ''' permutation: Randomly permute a sequence or return a permuted range (ndarray).
                     If x is a multi-dimensional array, it is only shuffled along its first index.'''
     shuffled_indices = np.random.permutation(len(data))
     test_set_size = int(len(data) * test_ratio)
-    test_indices = shuffled_indices[:test_set_size] #return the “x-first” values
+    # return the “x-first” values
+    test_indices = shuffled_indices[:test_set_size]
     return data.iloc[test_indices]
 
 
-def log_to_parquet(path_file: str, file_cols:list, parquet_engine:str="fastparquet"):
+def log_to_parquet(path_file: str, file_cols: list, parquet_engine: str):
     # LOAD DATA FROM A LOG FILE AND SAVE IT ON A PARQUET FILE TO IMPROVE PERFORMANCE AT READING THE DATA
-    file_name = path_file.split('.log')[0]+'.parquet'
+    file_name = path_file.split('.log')[0]+'.parq'
     df = read_csv(path_file, sep="\t", header=None,
-                    names=file_cols, low_memory=False)
+                  names=file_cols, low_memory=False)
     df.to_parquet(file_name, index=False, engine=parquet_engine)
     del df
     return file_name
-        
+
 
 #---------------------------------------------------------------------------------------------
 
 def conn_analysis():
     log_col_names = ["ts", "uid", "id_orig_h", "id_orig_p", "id_resp_h", "id_resp_p", "proto", "service", "duration", "orig_bytes", "resp_bytes",
-                        "conn_state", "local_orig", "missed_bytes", "history", "orig_pkts", "orig_ip_bytes", "resp_pkts", "resp_ip_bytes", "tunnel_parents"]
-    PARQUET_ENGINE = "fastparquet"
-    parquet_file = log_to_parquet(path_file="conn.log",file_cols=log_col_names)
-    complete_df = read_parquet(parquet_file, engine=PARQUET_ENGINE)
+                     "conn_state", "local_orig", "missed_bytes", "history", "orig_pkts", "orig_ip_bytes", "resp_pkts", "resp_ip_bytes", "tunnel_parents"]
+    P_ENGINE = "pyarrow"
+    parquet_file = log_to_parquet(
+        path_file="conn.log", file_cols=log_col_names, parquet_engine=P_ENGINE)
+    complete_df = read_parquet(parquet_file, engine=P_ENGINE)
     sample_df = get_random_sample_data(complete_df)
     del complete_df
-    sample_df.to_parquet("conn_sample.parquet",index=False, engine=PARQUET_ENGINE)
+    sample_df.to_parquet("conn_sample.parq",
+                         index=False, engine=P_ENGINE)
     df = sample_df
-    
-    # complete_df = read_parquet("conn.parquet")
 
-    # df = read_parquet("conn_sample.parquet")
+    # df = read_parquet("conn.parq")
+    # df = read_parquet("conn_sample.parq")
 
     df["ts"] = df["ts"].astype("float")
-    df["ts"] = list(map(lambda date: dt.fromtimestamp(date), df["ts"].tolist()))
-    df["duration"] = list(map(lambda dur: float(dur) if not "-" in dur else 0.0, df["duration"].tolist()))
+    df["ts"] = list(map(lambda date: dt.fromtimestamp(date),
+                        df["ts"].tolist()))
+    df["duration"] = list(map(lambda dur: float(dur) if not "-" in dur else 0.0,
+                            df["duration"].tolist()))
     print(df.info())
 
+    important_cols = ["ts", "id_orig_h", "id_resp_p", "duration"]
 
-    important_cols = ["ts","id_orig_h", "id_resp_p", "duration"]
+    df_not_web_port = df[(df["id_resp_p"] != 80) &
+                         (df["id_resp_p"] != 8080)].copy()
+    df_not_web_port.reset_index(drop=True, inplace=True)
 
-    df_not_web_port = df[(df["id_resp_p"]!=80) & (df["id_resp_p"]!=8080)].copy()
-    df_not_web_port.reset_index(drop=True,inplace=True)
-
-    print('\n',"Not http ports: ")
+    print('\n', "Not http ports: ")
     pprint(df_not_web_port[important_cols])
 
-    df_gp_not_web_port = df_not_web_port.groupby(important_cols[1:3]).size().to_frame().reset_index()
-    df_gp_not_web_port.rename(columns={0:"count"},inplace=True)
-    df_gp_not_web_port.sort_values(by="count",ascending=False,inplace=True)
+    df_gp_not_web_port = df_not_web_port.groupby(
+        important_cols[1:3]).size().to_frame().reset_index()
+    df_gp_not_web_port.rename(columns={0: "count"}, inplace=True)
+    df_gp_not_web_port.sort_values(by="count", ascending=False, inplace=True)
 
-
-    print('\n',"Not http ports groupby: ")
+    print('\n', "Not http ports groupby: ")
     pprint(df_gp_not_web_port)
 
-
-    df_long_conn = df[df["duration"]>5].copy()
-    df_long_conn.sort_values(by="duration",ascending=False,inplace=True)
-    df_long_conn.reset_index(drop=True,inplace=True)
-    print('\n',"Long duration connections: ")
+    df_long_conn = df[df["duration"] > 5].copy()
+    df_long_conn.sort_values(by="duration", ascending=False, inplace=True)
+    df_long_conn.reset_index(drop=True, inplace=True)
+    print('\n', "Long duration connections: ")
     pprint(df_long_conn[important_cols])
+
 
 def main():
     intiTime = time()
 
     conn_analysis()
-        
+
     elapsedTime = round(time()-intiTime, 2)
-    elapsedTime = str(elapsedTime/60) + "m" if elapsedTime >= 60 else str(elapsedTime)+"s"
+    elapsedTime = str(elapsedTime/60) + \
+        "m" if elapsedTime >= 60 else str(elapsedTime)+"s"
     print("\nTiempo del proceso --->", elapsedTime)
     sys.exit()
+
 
 main()
--- Act1-log_Analysis/README.md
+++ Act1-log_Analysis/README.md
@@ -1,3 +1,6 @@
 ## ACT 1 - Log Analysis
-Run
+
+After cloning this repo, dowload the ***conn.log*** file in the same folder of ***Log_analysis.py***<br>
+***conn.log*** --> https://experiencia21.tec.mx/courses/112876/files/42291245/download?download_frd=1<br>
+<br>Run:
 > python Log_analysis.py
