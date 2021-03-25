8bcc8a278eae247ae365036140d089a165476405
BuildTools <ercv106@hotmail.com>
2021-03-24 19:57:09 -0600

    24/03/2021 19:57:09

---
+++ Act2-Anomalous_Detection/README.md
@@ -0,0 +1,31 @@
+# ACT 1 - Log Analysis
+
+## Considerations
+
+After cloning this repo, dowload the file [http.log](vhttps://experiencia21.tec.mx/courses/112876/files/44700556/download?download_frd=1) in the same folder of `anomalous_detection.py`.
+
+## Execution
+
+First make sure that the requirements are correctly installed. The dependencies can be installed via: 
+
+`Anaconda`
+
+```shell
+~$ conda install -c defaults -c conda-forge --file requirements.txt
+```
+
+`pip`
+
+```shell
+~$ pip install -r requirements.txt
+```
+
+After getting the requirements run the below command to watch the data
+analysis.
+
+```shell
+~$ python anomalous_detection.py
+```
+
+If you prefer you can also execute the proyect with this: 
+`anomalous_detection.ipynb` Jupyter notebook.
---
+++ Act2-Anomalous_Detection/anomalous_detection.py
@@ -0,0 +1,59 @@
+from time import time
+from os import listdir
+from pandas import read_parquet, read_csv
+from datetime import datetime as dt
+
+def log_to_parquet(in_file: str, out_file: str, file_cols: list, parquet_engine: str):
+    # LOAD DATA FROM A LOG FILE AND SAVE IT ON A PARQUET FILE TO IMPROVE PERFORMANCE AT READING THE DATA
+    df = read_csv(in_file, sep="\t", header=None,
+                  names=file_cols, low_memory=False)
+    df.to_parquet(out_file, index=False, engine=parquet_engine)
+    del df
+
+
+def get_files_inFolder(folder: str, fileType: str):
+    return list(filter(lambda fileName: 
+                            fileName[-len(fileType):] == fileType,
+                        listdir(folder)))
+
+
+def an_detect(http_log_name: str):
+    P_ENGINE = "pyarrow"
+    cols_log = ['ts', 'uid', 'id_orig_h', 'id_orig_p', 'id_resp_h', 'id_resp_p',
+                'trans_depth', 'method', 'host', 'uri', 'referrer', 'user_agent',
+                'request_body_len', 'response_body_len', 'status_code', 'status_msg',
+                'info_code', 'info_msg', 'filename', 'tags', 'username',
+                'password', 'proxied', 'orig_fuids', 'orig_mime_types', 'resp_fuids',
+                'resp_mime_types']
+    list_parq_files = get_files_inFolder("./","parq")
+    http_parq_name = (http_log_name.split('.')[0])+".parq"
+    if not http_parq_name in list_parq_files:
+        log_to_parquet(http_log_name, http_parq_name, cols_log, P_ENGINE)
+    
+    df = read_parquet(http_parq_name)
+    df["ts"] = list(map(
+                    lambda date: 
+                        dt.fromtimestamp(float(date)),
+                    df["ts"].tolist()))
+    print(df.info())
+    print(df)
+
+
+def main():
+    paths = {
+        "http":"http-1.log"
+    }
+
+    intiTime = time()
+
+    # log_analysis(log_file="conn.log", sample_data=False)
+    an_detect(http_log_name=paths["http"])
+
+
+    elapsedTime = round(time()-intiTime, 2)
+    elapsedTime = str(elapsedTime/60) + \
+        "m" if elapsedTime >= 60 else str(elapsedTime)+"s"
+    print("\nTiempo del proceso --->", elapsedTime)
+
+
+main()
